a42c3f496935acd0cf6f5946fa40fefd7a4f0a87
S1mpunz Nobphadon S <nobphadon.sr.64@ubu.ac.th>
2024-03-06 22:51:46 +0700

    add file

--- dstool_x/rubrop/urls.py
+++ dstool_x/rubrop/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.auth.views import LoginView,PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     # Authentication
@@ -47,6 +49,8 @@ urlpatterns = [
     # Upload & Download
     path('adminhub/<uuid:event_id>/storage/upload/image', views.upload_image_storage, name='upload_image_storage'),
     path('adminhub/<uuid:event_id>/storage/dowload', views.download_images_as_zip, name='download_images_as_zip'),
+    url(r'^media/?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
     
 ]
 
